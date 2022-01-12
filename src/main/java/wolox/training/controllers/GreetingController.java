b6915837db62397a4ffdeeb24ec4d828fd81fe65
f-medina <f.medina@accenture.com>
2022-01-12 17:22:39 -0300

    creating a view and controller

---
+++ src/main/java/wolox/training/controllers/GreetingController.java
@@ -0,0 +1,18 @@
+package wolox.training.controllers;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, @NotNull Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
