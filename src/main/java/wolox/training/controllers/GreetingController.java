d0b2ae48daad71a0ee016cf4bf6b4f214a308a3d
f-medina <f.medina@accenture.com>
2022-01-13 17:00:02 -0300

    extracts the constant and replaces the expression with the constant Name

--- src/main/java/wolox/training/controllers/GreetingController.java
+++ src/main/java/wolox/training/controllers/GreetingController.java
@@ -7,11 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+
 public class GreetingController {
 
+    private static final String NAME = "name";
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, @NotNull Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name=NAME, required=false, defaultValue="World") String name, @NotNull Model model) {
+        model.addAttribute(NAME, name);
         return "greeting";
     }
 
